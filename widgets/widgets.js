5166be5fb08902ecf0854afd3441e46d39a1e267
Hayden <haydendonald@gmail.com>
2022-04-28 13:18:36 +1000

    Removed volume widget
    It's not complete

--- widgets/widgets.js
+++ widgets/widgets.js
@@ -9,6 +9,6 @@ module.exports = [
     require("../widgets/buttonSelector.js"),
     require("../widgets/horizontalStack.js"),
     require("../widgets/verticalStack.js"),
-    require("../widgets/volume.js"),
+    //require("../widgets/volume.js"),
     require("../widgets/actionButton.js")
 ]
